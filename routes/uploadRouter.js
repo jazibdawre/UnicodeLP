7c9bf89d82cb274c429fe0f9b3c2c14b7c6bf6d7
Jazib Dawre <jazib980@gmail.com>
2020-10-23 23:36:09 +0530

    Add error responses to /viewFiles

--- routes/uploadRouter.js
+++ routes/uploadRouter.js
@@ -8,19 +8,12 @@ const path = require('path');
 const uploadRouter = express.Router();
 uploadRouter.use(bodyparser.json());
 
-uploadRouter.use(function (req, res, next) {
-	console.log('Upload Request successful');
-	next();
-});
-
 //To specify the folder to which files will be stored
 let storage = multer.diskStorage({
 	destination: (req, file, cb) => {
-		//file has info
 		//cb => callback
 		cb(null, 'uploads'); //to destination folder (public folder)
 	},
-
 	filename: (req, file, cb) => {
 		cb(null, file.originalname); //filename saved as original name on server side
 	},
@@ -36,20 +29,16 @@ uploadRouter
 		res.statusCode = 403;
 		res.end('GET operation not allowed on /uploadFile');
 	})
-
 	.post(authenticate.verifyUser, upload.single('File'), (req, res, next) => {
 		//upload single => single file to be uploaded
-
 		res.statusCode = 200;
 		res.setHeader('Content-Type', 'application/json');
 		res.json(req.file);
 	})
-
 	.put(authenticate.verifyUser, (req, res, next) => {
 		res.statusCode = 403;
 		res.end('PUT operation not allowed on /uploadFile');
 	})
-
 	.delete(authenticate.verifyUser, (req, res, next) => {
 		res.statusCode = 403;
 		res.end('DELETE operation not allowed on /uploadFile');
@@ -57,7 +46,6 @@ uploadRouter
 
 uploadRouter
 	.route('/download/:filename')
-
 	.get((req, res, next) => {
 		const file = path.join(
 			__dirname,
@@ -71,17 +59,14 @@ uploadRouter
 			}
 		});
 	})
-
 	.post(authenticate.verifyUser, (req, res, next) => {
 		res.statusCode = 403;
 		res.end('POST operation not allowed on /download');
 	})
-
 	.put(authenticate.verifyUser, (req, res, next) => {
 		res.statusCode = 403;
 		res.end('PUT operation not allowed on /download');
 	})
-
 	.delete(authenticate.verifyUser, (req, res, next) => {
 		res.statusCode = 403;
 		res.end('DELETE operation not allowed on /download');
@@ -94,6 +79,18 @@ uploadRouter
 		let dir_name = 'uploads';
 		let file_names = fs.readdirSync(dir_name);
 		res.send(file_names);
+	})
+	.post(authenticate.verifyUser, (req, res, next) => {
+		res.statusCode = 403;
+		res.end('POST operation not allowed on /viewFiles');
+	})
+	.put(authenticate.verifyUser, (req, res, next) => {
+		res.statusCode = 403;
+		res.end('PUT operation not allowed on /viewFiles');
+	})
+	.delete(authenticate.verifyUser, (req, res, next) => {
+		res.statusCode = 403;
+		res.end('DELETE operation not allowed on /viewFiles');
 	});
 
 module.exports = uploadRouter;
